3bc04ae6f6e75c0537aacad4212683280a3ad49f
Georgy <geravos2@gmail.com>
2022-03-26 13:11:08 +0300

    Готовый второй урок

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = "JS code"
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 30
+let rollback = 27
+let fullPrice = 10.0
+let adaptive = true
 alert("Hello world !");
-console.log("Hello console!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
