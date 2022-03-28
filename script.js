043671be27b97e276d54139c2c1b4e68aee622b3
Georgy <geravos2@gmail.com>
2022-03-28 14:37:55 +0300

    6 пунктов

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = prompt("Сколько будет стоить данная работа?");
+let rollback = 27
+let fullPrice = 10.0
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 alert("Hello world !");
-console.log("Hello console!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
