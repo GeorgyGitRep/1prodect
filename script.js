b63b97e16ca4d755230d08c637234c47a7002e14
Georgy <geravos2@gmail.com>
2022-03-31 18:38:49 +0300

    Lesson04 готов

--- script.js
+++ script.js
@@ -7,26 +7,45 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let Otcat = 0;
+let fullPrice = 0;
 let servicePercentPrice = fullPrice - Otcat;
 servicePercentPrice = Math.ceil(servicePercentPrice);
-if (fullPrice > 30000)
-    console.log("Даем скидку в 10%");
-else if (fullPrice > 15000 && fullPrice <= 30000)
-    console.log("Даем скидку в 5%");
-else if (fullPrice <= 15000 && fullPrice >= 0)
-    console.log("Скидка не предусмотрена");
-else if (fullPrice < 0)
-    console.log("Что то пошло не так")
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " долларов");
-console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+const getRollbackMessage = function () {
+    if (fullPrice > 30000)
+        console.log("Даем скидку в 10%");
+    else if (fullPrice > 15000 && fullPrice <= 30000)
+        console.log("Даем скидку в 5%");
+    else if (fullPrice <= 15000 && fullPrice >= 0)
+        console.log("Скидка не предусмотрена");
+    else if (fullPrice < 0)
+        console.log("Что то пошло не так")
+}
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable)
+}
+let getAllServicePrices = function (servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2
+};
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices
+};
+function getTitle(title) {
+    title = title.toLowerCase().trim();
+    title = title.charAt(0).toUpperCase() + title.slice(1);
+    return title
+};
+function getServicePercentPrices(fullPrice, Otcat) {
+    return fullPrice - Otcat
+}
+title = getTitle(title);
+let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, Otcat);
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+getRollbackMessage();
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
 console.log(servicePercentPrice);
-alert("Hello world !");
+
